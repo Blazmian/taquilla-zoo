630b9df4ea29cf6b9834fce40716280d9595b7b4
MeraliAcuna <112598438+MeraliAcuna@users.noreply.github.com>
2023-12-03 19:24:46 -0700

    Tickets
    Buy tickets at the zoo box office.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,31 @@ import Logo from './img/zoo-logo.png';
 import LogoLetras from './img/logo-letras.png';
 import { Navbar } from 'react-bootstrap';
 import SelectTickets from './components/SelectTickets';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Tickets from './components/Tickets';
 
 const App = () => {
   return (
-    <>
-      <Navbar className="nav-body p-0">
-        <div className='ps-3 py-2 pe-5'>
-          <Navbar.Brand style={{ width: '30%' }}>
-            <img src={Logo} className='me-2' width={60} alt='Zoo-Logo' />
-            <img src={LogoLetras} width={100} alt='Zoo-Logo-Letras' />
-          </Navbar.Brand>
-        </div>
-      </Navbar>
-      <SelectTickets />
-    </>
-  )
-}
+    <Router>
+      <>
+        <Navbar className="nav-body p-0">
+          <div className='ps-3 py-2 pe-5'>
+            <Navbar.Brand style={{ width: '30%' }}>
+              <img src={Logo} className='me-2' width={60} alt='Zoo-Logo' />
+              <img src={LogoLetras} width={100} alt='Zoo-Logo-Letras' />
+            </Navbar.Brand>
+          </div>
+        </Navbar>
 
-export default App
+       
+      <Routes>
+        <Route path="/seleccionar-ticket" element={<SelectTickets />} />
+        <Route path="/taquilla" element={<Tickets />} />
+      </Routes>
+ 
+      </>
+    </Router>
+  );
+};
+
+export default App;
